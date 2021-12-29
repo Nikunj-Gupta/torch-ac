c6d4aafe3b4602630894d038d7f6ed1990ed4622
Nikunj Gupta <ng2531@nyu.edu>
2021-12-28 20:19:28 -0500

    updated inits to initialize new classes corresponding to voi

--- torch_ac/__init__.py
+++ torch_ac/__init__.py
@@ -1,3 +1,3 @@
-from torch_ac.algos import A2CAlgo, PPOAlgo
+from torch_ac.algos import A2CAlgo, PPOAlgo, PPOAlgoOriginal
 from torch_ac.model import ACModel, RecurrentACModel
 from torch_ac.utils import DictList
