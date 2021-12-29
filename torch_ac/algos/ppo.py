4b5a8e9229f6ace7b4ed9911a84408ca2a40a59c
Nikunj Gupta <ng2531@nyu.edu>
2021-12-28 20:18:16 -0500

    updated to support voi

--- torch_ac/algos/ppo.py
+++ torch_ac/algos/ppo.py
@@ -1,6 +1,7 @@
-import numpy
+import numpy 
 import torch
-import torch.nn.functional as F
+import torch.nn.functional as F 
+torch.autograd.set_detect_anomaly(True)
 
 from torch_ac.algos.base import BaseAlgo
 
@@ -19,11 +20,16 @@ class PPOAlgo(BaseAlgo):
 
         self.clip_eps = clip_eps
         self.epochs = epochs
-        self.batch_size = batch_size
+        self.batch_size = batch_size 
 
-        assert self.batch_size % self.recurrence == 0
+        assert self.batch_size % self.recurrence == 0 
 
-        self.optimizer = torch.optim.Adam(self.acmodel.parameters(), lr, eps=adam_eps)
+        self.parameter_set1 = [param for name, param in self.acmodel.named_parameters() if 'hammer' not in name and 'voi' not in name] 
+        self.parameter_set2 = [param for name, param in self.acmodel.named_parameters() if 'hammer' in name or 'voi' in name]  
+
+        self.optimizer = torch.optim.Adam(self.parameter_set1, lr, eps=adam_eps)
+        self.optimizer2 = torch.optim.Adam(self.parameter_set2, lr, eps=adam_eps)
+
         self.batch_num = 0
 
     def update_parameters(self, exps):
@@ -34,18 +40,26 @@ class PPOAlgo(BaseAlgo):
 
             log_entropies = []
             log_values = []
+            log_costs = []
             log_policy_losses = []
             log_value_losses = []
+            log_policy_losses2 = []
+            log_value_losses2 = []
             log_grad_norms = []
+            log_grad_norms2 = []
 
             for inds in self._get_batches_starting_indexes():
                 # Initialize batch values
 
                 batch_entropy = 0
                 batch_value = 0
+                batch_costs = 0
                 batch_policy_loss = 0
                 batch_value_loss = 0
+                batch_policy_loss2 = 0
+                batch_value_loss2 = 0
                 batch_loss = 0
+                batch_loss2 = 0
 
                 # Initialize memory
 
@@ -60,31 +74,46 @@ class PPOAlgo(BaseAlgo):
                     # Compute loss
 
                     if self.acmodel.recurrent:
-                        dist, value, memory = self.acmodel(sb.obs, memory * sb.mask)
+                        dist, value, cost, memory = self.acmodel(sb.obs, memory * sb.mask)
                     else:
-                        dist, value = self.acmodel(sb.obs)
+                        dist, value, cost = self.acmodel(sb.obs)
 
                     entropy = dist.entropy().mean()
 
                     ratio = torch.exp(dist.log_prob(sb.action) - sb.log_prob)
+                    
                     surr1 = ratio * sb.advantage
                     surr2 = torch.clamp(ratio, 1.0 - self.clip_eps, 1.0 + self.clip_eps) * sb.advantage
                     policy_loss = -torch.min(surr1, surr2).mean()
+                    
+                    surr1_2 = ratio * sb.advantage2
+                    surr2_2 = torch.clamp(ratio, 1.0 - self.clip_eps, 1.0 + self.clip_eps) * sb.advantage2
+                    policy_loss2 = -torch.min(surr1_2, surr2_2).mean()
 
                     value_clipped = sb.value + torch.clamp(value - sb.value, -self.clip_eps, self.clip_eps)
+                    
                     surr1 = (value - sb.returnn).pow(2)
                     surr2 = (value_clipped - sb.returnn).pow(2)
                     value_loss = torch.max(surr1, surr2).mean()
+                    
+                    surr1_2 = (value - sb.returnn2).pow(2)
+                    surr2_2 = (value_clipped - sb.returnn2).pow(2)
+                    value_loss2 = torch.max(surr1_2, surr2_2).mean()
 
                     loss = policy_loss - self.entropy_coef * entropy + self.value_loss_coef * value_loss
+                    loss2 = policy_loss2 - self.entropy_coef * entropy + self.value_loss_coef * value_loss2
 
                     # Update batch values
 
                     batch_entropy += entropy.item()
                     batch_value += value.mean().item()
+                    batch_costs += cost.mean().item()
                     batch_policy_loss += policy_loss.item()
                     batch_value_loss += value_loss.item()
+                    batch_policy_loss2 += policy_loss2.item()
+                    batch_value_loss2 += value_loss2.item()
                     batch_loss += loss
+                    batch_loss2 += loss2
 
                     # Update memories for next epoch
 
@@ -95,34 +124,57 @@ class PPOAlgo(BaseAlgo):
 
                 batch_entropy /= self.recurrence
                 batch_value /= self.recurrence
+                batch_costs /= self.recurrence
                 batch_policy_loss /= self.recurrence
                 batch_value_loss /= self.recurrence
+                batch_policy_loss2 /= self.recurrence
+                batch_value_loss2 /= self.recurrence
                 batch_loss /= self.recurrence
+                batch_loss2 /= self.recurrence
 
                 # Update actor-critic
 
+                
+                
                 self.optimizer.zero_grad()
-                batch_loss.backward()
-                grad_norm = sum(p.grad.data.norm(2).item() ** 2 for p in self.acmodel.parameters()) ** 0.5
-                torch.nn.utils.clip_grad_norm_(self.acmodel.parameters(), self.max_grad_norm)
+                batch_loss.backward(retain_graph=True)
+                grad_norm = sum(p.grad.data.norm(2).item() ** 2 for p in self.parameter_set1) ** 0.5
+                torch.nn.utils.clip_grad_norm_(self.parameter_set1, self.max_grad_norm)
+                
+
+            
+                self.optimizer2.zero_grad()
+                batch_loss2.backward()
+                grad_norm2 = sum(p.grad.data.norm(2).item() ** 2 for p in self.parameter_set2) ** 0.5
+                torch.nn.utils.clip_grad_norm_(self.parameter_set2, self.max_grad_norm)
+                
                 self.optimizer.step()
+                self.optimizer2.step()
 
                 # Update log values
 
                 log_entropies.append(batch_entropy)
                 log_values.append(batch_value)
+                log_costs.append(batch_costs)
                 log_policy_losses.append(batch_policy_loss)
                 log_value_losses.append(batch_value_loss)
+                log_policy_losses2.append(batch_policy_loss2)
+                log_value_losses2.append(batch_value_loss2)
                 log_grad_norms.append(grad_norm)
+                log_grad_norms2.append(grad_norm2)
 
         # Log some values
 
         logs = {
             "entropy": numpy.mean(log_entropies),
             "value": numpy.mean(log_values),
+            "cost": numpy.mean(log_costs),
             "policy_loss": numpy.mean(log_policy_losses),
             "value_loss": numpy.mean(log_value_losses),
-            "grad_norm": numpy.mean(log_grad_norms)
+            "policy_loss2": numpy.mean(log_policy_losses2),
+            "value_loss2": numpy.mean(log_value_losses2),
+            "grad_norm": numpy.mean(log_grad_norms), 
+            "grad_norm2": numpy.mean(log_grad_norms2)
         }
 
         return logs
